048d08313681c317416df9fd730dc29d18a258e0
calebfonyuy <fonyuyca@gmail.com>
2020-01-01 01:07:05 +0100

    Complete Front end

--- knowledge_search/settings.py
+++ knowledge_search/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
         'HOST': '127.0.0.1',
         'NAME': 'knowledge_search',
         'USER': 'root',
-        'PASSWORD': 'djoudken'
+        'PASSWORD': 'kitiwum2@BUI'
     }
 }
 
