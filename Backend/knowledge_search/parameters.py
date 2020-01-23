9b13a41aab1548e133dbb9cb8cd8508de0af58c7
calebfonyuy <fonyuyca@gmail.com>
2020-01-23 09:01:53 +0100

    Final Project

--- Backend/knowledge_search/parameters.py
+++ Backend/knowledge_search/parameters.py
@@ -1,6 +1,6 @@
 THRESHOLD = 10 #Minimum number of occurences of search term in the web page
 
-API_KEY = 'AIzaSyAkYuCBKAgKDDPW7oveuDg5g3aqjdf4euU'
+API_KEY = 'AIzaSyC5cgMZBjFaFU3M7Lzo9iSyQvqEiKhy3YQ'
 
 CX = '008459376045476562504:i56qn30nuh7'
 
